a343ab649a99ee4ecd8a4616bd78fc2166d4858a
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-08-02 22:39:20 +0530

    Setup On Click For Sign In And Sign Out

--- public/app.js
+++ public/app.js
@@ -1,1 +1,10 @@
-console.log(firebase)
+const auth = firebase.auth()
+
+const signInButton = document.getElementById('sign_in')
+const signOutButton = document.getElementById('sign_out')
+
+const provider = new firebase.auth.GoogleAuthProvider()
+
+signInButton.onclick = () => auth.signInWithPopup(provider)
+
+signOutButton.onclick = () => auth.signOut()
