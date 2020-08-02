3de50ebcc8a94b25e525a1386f0dcce753d0ed9c
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-08-02 22:49:28 +0530

    Listening To Auth State Changes

--- public/app.js
+++ public/app.js
@@ -1,5 +1,10 @@
 const auth = firebase.auth()
 
+const whenSignedIn = document.getElementById('when_signed_in')
+const whenSignedOut = document.getElementById('when_signed_out')
+
+const userDetails = document.getElementById('user_details')
+
 const signInButton = document.getElementById('sign_in')
 const signOutButton = document.getElementById('sign_out')
 
@@ -8,3 +13,18 @@ const provider = new firebase.auth.GoogleAuthProvider()
 signInButton.onclick = () => auth.signInWithPopup(provider)
 
 signOutButton.onclick = () => auth.signOut()
+
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>${user.displayName}</h3>`
+
+    } else {
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = ``
+
+    }
+})
